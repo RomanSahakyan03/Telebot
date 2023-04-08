cb9001e53a0a38420f7d46b0509afdf2eeedbc74
RomanSahakyan03 <sahakyanroman.a@gmail.com>
2023-04-08 20:00:14 +0400

    some changes

--- TypeTalk/app.py
+++ TypeTalk/app.py
@@ -2,6 +2,8 @@ import json
 import time
 import requests
 
+from bot import BotDB
+db = BotDB("/userdata.db")
 
 # Set up API endpoint and bot token
 API_LINK = "https://api.telegram.org/bot6114753472:AAFBAES3t622glVzoe5-4BpKF0hjbBeX6_c"
@@ -10,9 +12,9 @@ SEND_MESSAGE_URL = f"{API_LINK}/sendMessage"
 INLINE_KEYBOARD = f"{API_LINK}/InlineKeyboardMarkup"
 SEND_PHOTO_URL = f"{API_LINK}/sendPhoto"
 PROCCESSED_OFFSET = 0
+USER_STATE = {} # Can be 'age_collect', 'age_interval', 'region_collect'
 
-
-# Define constants for string literals
+# Define constants for string literals for localisation 
 START_MESSAGE = "👋 Start The Conversation"
 SETTINGS_MESSAGE = "🛠️ Manage your Settings"
 ABOUT_MESSAGE = "📖About the bot"
@@ -30,10 +32,16 @@ def join(update):
 
 # Define a function to handle the /start command
 def start(update):
-    if(None):
-        start(update)
-    else:    
+    chat_id = update['message']['chat']['id']
+    if db.check_exist(chat_id):
         join(update)
+    settings_queue = ['language', 'TYPE', 'TYPES', 'region', 'age', 'min_prefered_age', 'max_prefered_age']
+    dict = {}
+    
+    #if 'language' not in dict:
+    #    callback_handler('language')
+    #    dict.update({'language' : })
+    #    start(update, dict)
 
 # Define a function to handle the /about command
 def about(update):
@@ -47,7 +55,6 @@ def about(update):
 
         "resize_keyboard": True
     }
-# MARIA's chat id 842079224
 
     data = {
         "chat_id": chat_id,
@@ -74,13 +81,97 @@ def stop(update):
     else:
         print('Failed to Stop Conversation.')
 
+def lang_setting(update):
+    chat_id = update['callback_query']['from']['id']
+    keyboard = {
+    "inline_keyboard": [
+        [{"text": "🇺🇸 English", "callback_data": "English"},
+        {"text": "🇷🇺 Russian", "callback_data": "Russian"}]
+        ]}
+    data = {
+        "chat_id" : chat_id,
+        "text" : "Preferred language?",
+        "reply_markup" : json.dumps(keyboard)
+    }
+    response = requests.post(SEND_MESSAGE_URL, json=data, timeout=1.5)
+    if response.ok:
+        print('Language selection menu opened successfully.')
+    else:
+        print('Failed to open language selection menu.')
+
+def TYPE_setting(update):
+    chat_id = update['callback_query']['from']['id']
+    mbti_types = {
+    0: "INTJ", 1: "INFJ", 2: "ISTJ", 3: "ISTP",
+    4: "INTP", 5: "INFP", 6: "ISFJ", 7: "ISFP",
+    8: "ENTJ", 9: "ENFJ", 10: "ESTJ", 11: "ESTP",
+    12: "ENTP", 13: "ENFP", 14: "ESFJ", 15: "ESFP"
+    }
+    # Create the keyboard
+    keyboard = {
+    "inline_keyboard": [
+        [{"text": mbti_types[bit], "callback_data": mbti_types[bit] + "_m"} for bit in range(row * 4, (row + 1) * 4) if bit < 16] for row in range((16 - 1) // 4 + 1)
+    ]
+    }
+    # Convert the keyboard to JSON format
+    keyboard_json = json.dumps(keyboard)
+    data = {
+        "chat_id" : chat_id,
+        "text" : "Choose your MBTI type",
+        "reply_markup" : json.dumps(keyboard)
+    }
+
+    response = requests.post(SEND_MESSAGE_URL, json=data, timeout=1.5)
+    if response.ok:
+        print('MBTI type selection menu opened successfully.')
+    else:
+        print('Failed to open MBTI type selection menu.')
+
+
+def TYPES_setting(update):
+    chat_id = update['callback_query']['from']['id']
+    mbti_types = {
+    0: "INTJ", 1: "INFJ", 2: "ISTJ", 3: "ISTP",
+    4: "INTP", 5: "INFP", 6: "ISFJ", 7: "ISFP",
+    8: "ENTJ", 9: "ENFJ", 10: "ESTJ", 11: "ESTP",
+    12: "ENTP", 13: "ENFP", 14: "ESFJ", 15: "ESFP"
+    }
+    #x = db.select_parameter("users", "TYPES", f"chat_id = {chat_id}")
+    x = 1315
+    print(x)
+    num_bits = 16
+    # Create a list of the MBTI types, with checkmark emojis for the selected types
+    mbti_types_list = [f"✅ {mbti_types[bit]}" if (x >> bit) & 1 else f"  {mbti_types[bit]}" for bit in range(num_bits)]
+
+    # Divide the MBTI types into rows of 4, and create the keyboard
+    keyboard = {
+    "inline_keyboard": [
+        [{"text": mbti_types_list[bit], "callback_data": mbti_types[bit] + "_p"} for bit in range(row * 4, (row + 1) * 4) if bit < num_bits] for row in range((num_bits - 1) // 4 + 1)
+    ]
+}
+    # Convert the keyboard to JSON format
+    keyboard_json = json.dumps(keyboard)
+    data = {
+        "chat_id" : chat_id,
+        "text" : "Choose your preffered MBTI types",
+        "reply_markup" : json.dumps(keyboard)
+    }
+
+    response = requests.post(SEND_MESSAGE_URL, json=data, timeout=1.5)
+    if response.ok:
+        print('MBTI preffered types selection menu opened successfully.')
+    else:
+        print('Failed to open MBTI preffered types selection menu.')
+
+
 def settings(update):
     chat_id = update['message']['chat']['id']
     # Create the keyboard
     keyboard = {
     "inline_keyboard": [
-        [{"text": "Your MBTI Type", "callback_data": "my_type"},
-        {"text": "Prefered MBTI Types", "callback_data": "prefered_types"},],
+        [{"text": "Language", "callback_data": "language"},],
+        [{"text": "Your MBTI Type", "callback_data": "TYPE"},
+        {"text": "Prefered MBTI Types", "callback_data": "TYPES"},],
         [{"text": "Your Age", "callback_data": "my_age"},
         {"text": "Prefered Age Interval", "callback_data": "prefered_ages"},],
         [{"text": "💸Gender", "callback_data": "genders"},
@@ -98,15 +189,25 @@ def settings(update):
         print('Failed to open Settings.')
 
 
-def callback_handler(call_back_data):
-    print(call_back_data)
-    if call_back_data == "my_type":
+def callback_handler(update):
+    callback_data = update['callback_query']['data']
+    print(callback_data)
+    if callback_data == "language":
+        lang_setting(update)
+        return
+    if callback_data == "TYPE":
+        TYPE_setting(update)
+        return
+    if callback_data == "TYPES":
+        TYPES_setting(update)
+        return
+    if callback_data == "region":
         pass
-    if call_back_data == "prefered_types":
+    if callback_data == "age":
         pass
-    if call_back_data == "genders":
+    if callback_data == "min_prefered_age":
         pass
-    if call_back_data == "region":
+    if callback_data == "max_prefered_age":
         pass
 
 
@@ -136,10 +237,8 @@ while True:
                         if handler:
                             handler(update)
                 if 'callback_query' in update and 'data' in update['callback_query']:
-                    callback_data = update['callback_query']['data']
-                    if 'data' in update['callback_query']:
-                        callback_handler(callback_data)
-            PROCCESSED_OFFSET = max(PROCCESSED_OFFSET, update['update_id'] + 1)
+                    callback_handler(update)              
+                PROCCESSED_OFFSET = max(PROCCESSED_OFFSET, update['update_id'] + 1)
         time.sleep(0.75)
     except Exception as e:
         print(f'Error occurred: {e}')
--- TypeTalk/bot.py
+++ TypeTalk/bot.py
@@ -1,12 +1,22 @@
 from db import BotDB
 
-db = BotDB("/dbs/userdata.db")
-db.create_table('users', ['id INTEGER PRIMARY KEY', 'chat_id INTEGER', 'language TEXT',
+db = BotDB("./userdata.db")
+db.create_table('users', ['id INTEGER PRIMARY KEY AUTOINCREMENT', 'chat_id INTEGER', 'language TEXT',
                           'TYPE INTEGER(1)', 'TYPES INTEGER(2)', 'region TEXT',
-                          'age INTEGER(1)', 'min_prefered_age INTEGER(1)', 'max_prefered_age INTEGER(1)',
-                          ''])
+                          'age INTEGER(1)', 'min_prefered_age INTEGER(1)', 'max_prefered_age INTEGER(1)'])
 
-genders = BotDB('/dbs/genders.db')
-genders.create_table('gender_preferences', ['id INTEGER PRIMARY KEY', 'user_id INTEGER' ,
-                                            'gender TEXT' , 'preferred_genders TEXT', 
-                                            'FOREIGN KEY(user_id) REFERENCES users(user_id)'])
+db.create_table('users', ['id INTEGER PRIMARY KEY AUTOINCREMENT', 
+                          'chat_id INTEGER', 
+                          'language TEXT',
+                          'TYPE INTEGER(1)', 
+                          'TYPES INTEGER(2)', 
+                          'region_lat TEXT',
+                          'region_lon TEXT',
+                          'age INTEGER(1)', 
+                          'age_interval TEXT',
+                          'rate INTEGER(1)',
+                          'premium INTEGER(1)'])
+
+db.create_table('gender_preferences', ['id INTEGER PRIMARY KEY AUTOINCREMENT', 'chat_id INTEGER' ,
+                                        'gender TEXT' , 'preferred_genders TEXT', 
+                                        'FOREIGN KEY(chat_id) REFERENCES users(chat_id)'])
