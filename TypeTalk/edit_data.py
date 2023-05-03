d9402aedd6ece7506bf1241e49f42ca169c5e8e9
RomanSahakyan03 <sahakyanroman.a@gmail.com>
2023-05-03 22:01:55 +0400

    dv

---
+++ TypeTalk/edit_data.py
@@ -0,0 +1,59 @@
+import requests
+import redis
+
+cache = redis.Redis(host='localhost', port=6379, db=0)
+API_LINK = "https://api.telegram.org/bot6114753472:AAFBAES3t622glVzoe5-4BpKF0hjbBeX6_c"
+EDIT_MESSAGE_URL = f"{API_LINK}/editMessageText"
+EDIT_CAPTION_URL = f"{API_LINK}/editMessageCaption"
+EDIT_MEDIA_URL = f"{API_LINK}/editMessageMedia"
+
+
+def edit_message(update):
+    message_id = update["edited_message"]["message_id"]
+    receiver = cache.hget('pairs', update["edited_message"]["from"]["id"]).decode()
+    data = {
+        'chat_id' : receiver,
+        'message_id' : cache.hget(str(receiver), message_id).decode(),
+        'text' : update['edited_message']['text']
+    }
+    response = requests.post(EDIT_MESSAGE_URL, json=data)
+    if response.ok:
+        print(f"edited message in {receiver} successfully")
+    else:
+        print(f"failed to edited message for {receiver}")
+
+def edit_media(update):
+    message_id = update["edited_message"]["message_id"]
+    receiver = cache.hget('pairs', update["edited_message"]["from"]["id"]).decode()
+    data = {
+        'chat_id' : receiver,
+        'message_id' : cache.hget(str(receiver), message_id).decode(),
+        'media' : {}
+    }
+    if 'photo' in update['edited_message']:
+        print('in photo')
+        data['media']['type'] = 'photo'
+        data['media']['media'] = update['edited_message']['photo'][-1]['file_id']
+    elif 'video' in update['edited_message']:
+        data['media']['type'] = 'video'
+        data['media']['media'] = update['edited_message']['video']['file_id']
+    elif 'animation' in update['edited_message']:
+        data['media']['type'] = 'animation'
+        data['media']['media'] = update['edited_message']['animation']['file_id']
+    elif 'document' in update['edited_message']:
+        data['media']['type'] = 'document'
+        data['media']['media'] = update['edited_message']['document']['file_id']
+    elif 'audio' in update['edited_message']:
+        data['media']['type'] = 'audio'
+        data['media']['media'] = update['edited_message']['voice']['file_id']
+
+    if 'caption' in update['edited_message']:
+        print('in caption')
+        data['media']['caption'] = update['edited_message']['caption']
+    print(data['media'])
+    print(data['message_id'])
+    response = requests.post(EDIT_MEDIA_URL, json=data)
+    if response.ok:
+            print(f"edited media in {receiver} successfully")
+    else:
+            print(f"failed to edited media for {receiver}")
--- TypeTalk/send_data.py
+++ TypeTalk/send_data.py
@@ -1,6 +1,7 @@
-import json
 import requests
+import redis
 
+cache = redis.Redis(host='localhost', port=6379, db=0)
 API_LINK = "https://api.telegram.org/bot6114753472:AAFBAES3t622glVzoe5-4BpKF0hjbBeX6_c"
 SEND_MESSAGE_URL = f"{API_LINK}/sendMessage"
 SEND_PHOTO_URL = f"{API_LINK}/sendPhoto"
@@ -14,17 +15,24 @@ SEND_STICKER_URL = f"{API_LINK}/sendSticker"
 SEND_POLL_URL = f"{API_LINK}/sendPoll"
 
 def send_message(receiver, update):
-    chat_id = update['message']['from']['id']
     text = update['message']['text']
     data = {
             "chat_id": receiver,
             "text": text,
         }
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
+        
     response = requests.post(SEND_MESSAGE_URL, json=data)
     if response.ok:
+        chat_id = update['message']['from']['id']
         print(f"message sent successfully from {chat_id} to {receiver}")
+        response_data = response.json()
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"failed to send message from {chat_id} to {receiver}")
 
@@ -37,11 +45,18 @@ def send_photo(receiver, update):
     if "caption" in update["message"]:
         data["caption"] = update["message"]["caption"]
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
     
     response = requests.post(SEND_PHOTO_URL, json=data)
     if response.ok:
         print(f"Photo sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Photo message.")
 
@@ -57,6 +72,12 @@ def send_audio(receiver, update):
     response = requests.post(SEND_AUDIO_URL, json=data)
     if response.ok:
         print(f"Audio sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Audio message.")
 
@@ -68,12 +89,18 @@ def send_document(receiver, update):
     if "caption" in update["message"]:
         data["caption"] = update["message"]["caption"]
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
     
-
     response = requests.post(SEND_DOCUMENT_URL, json=data)
     if response.ok:
         print(f"Document sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Document message.")
 
@@ -86,11 +113,18 @@ def send_video(receiver, update):
     if "caption" in update["message"]:
         data["caption"] = update["message"]["caption"]
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
 
     response = requests.post(SEND_VIDEO_URL, json=data)
     if response.ok:
         print(f"Video sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Video message.")   
 
@@ -103,10 +137,17 @@ def send_animation(receiver, update):
     if "caption" in update["message"]:
         data["caption"] = update["message"]["caption"]
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
     response = requests.post(SEND_ANIMATION_URL, json=data)
     if response.ok:
         print(f"Animation sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Animation message.")
 
@@ -119,11 +160,18 @@ def send_location(receiver, update):
         'longitude' : longitude,        
     }
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
 
     response = requests.post(SEND_LOCATION_URL, json=data)
     if response.ok:
         print(f"Location sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Location message.")
     
@@ -134,11 +182,18 @@ def send_video_note(receiver, update):
         "video_note" : video_note,
     }
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
 
     response = requests.post(SEND_VIDEONOTE_URL, json=data)
     if response.ok:
         print(f"VideoNote sent to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} VideoNote message.")
 
@@ -148,11 +203,18 @@ def send_sticker(receiver, update):
         "sticker" : update["message"]["sticker"]["file_id"]
     }
     if "reply_to_message" in update["message"]:
-        data["reply_to_message_id"] = update["message"]["reply_to_message"]["message_id"]
+        message_id = update["message"]["reply_to_message"]["message_id"]
+        data["reply_to_message_id"] = cache.hget(str(receiver), message_id).decode()
 
     response = requests.post(SEND_STICKER_URL, json=data)
     if response.ok:
         print(f"Stiker sent from to {receiver} successfully.")
+        response_data = response.json()
+        chat_id = update['message']['from']['id']
+        message_id1 = update['message']['message_id']
+        message_id2 = response_data['result']['message_id']
+        cache.hset(str(receiver), message_id1, message_id2)
+        cache.hset(str(chat_id), message_id2, message_id1)
     else:
         print(f"Failed to send {receiver} Sticker message.")
 
